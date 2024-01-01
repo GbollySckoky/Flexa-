5a4b3994aa0f79a862c116b6fcf8e1bc4f90a244
Coker <gbolahancoker99@gmail.com>
2024-01-01 23:08:17 +0100

    Flexa_Project

--- src/components/Home/Features.jsx
+++ src/components/Home/Features.jsx
@@ -1,18 +1,20 @@
 import img8 from "../../assets/Group 8.svg";
 import rectangle from "../../assets/Rectangle.svg"
+import image24 from "../../assets/image 24.svg"
 import { Link } from "react-router-dom";
 import { MdArrowOutward } from "react-icons/md";
+import Work from "./Work";
 
 const Features = () => {
   return (
     <div className="bg-grey">
-      <div className="text-center pt-[30px] text-xl text-lightBlue  font-light">
+      <div className="text-center pt-[30px] text-xl text-lightBlue font-light">
         <h3>Features That Make Us Standout</h3>
       </div>
       <section className=" w-[85%] m-auto">
         {/* MANAGE A CARD FOR ALL BUSINESS EXPENSES */}
 
-        <div className="mt-[50px]">
+        <div className="mt-[70px]">
           <h1 className="text-lightblues text-2xl font-medium pb-3">
             Manage A Card For All Business Expenses
           </h1>
@@ -49,7 +51,7 @@ const Features = () => {
 
         {/* GENERATE A FREE BANK ACCOUNT */}
 
-        <div className="mt-[50px]">
+        <div className="mt-[70px]">
           <h1 className="text-lightblues text-2xl font-medium pb-3">
             Generate A Free Bank Account
           </h1>
@@ -85,16 +87,16 @@ const Features = () => {
 
           {/* Flexible Loans */}
 
-          <div className="mt-[50px]">
+          <div className="mt-[70px]">
           <h1 className="text-lightblues text-2xl font-medium pb-3">
-            Generate A Free Bank Account
+            Flexible Loans
           </h1>
           <div className="flex justify-between ">
             <p className="w-[600px] text-lightBlue pb-5 font-normal">
-                Unlock the power of hassle-free banking with Flex`s complimentry bank account generation future.
-                in just a few clicks, create your own free bank account right within the Flexa platform.
+                At Flexa, we understand that financial needs vary, and flexibility is key when it comes to borrowing. 
+                That`s why we are proud to introduce our Flexible loans feature 
             </p>
-            <p className="text-lightBlue font-light">BANK ACCOUNT</p>
+            <p className="text-lightBlue font-light">LOANS</p>
           </div>
           <div className="bg-Blue pl-[50px]">
             <div className="flex ">
@@ -103,9 +105,9 @@ const Features = () => {
                 <h1 className="text-2xl font-normal pt-[60px] pb-[30px]">
                   Instant And Free Bank Account Creation
                 </h1>
-                <p>
-                  Quickly create your own account within Flexa without any associated costs, 
-                  eliminating the need for traditional banking fees. 
+                <p className="w-[500px]">
+                  Say goodbye to lengthly approval processes. Get an instant decision on your loan application,
+                  empowering you to make timely financial decisions.
                 </p>
                 </div>
                 <Link className="flex items-center text-yellows" to={"#"}>
@@ -113,14 +115,15 @@ const Features = () => {
                 </Link>
               </div>
               <div id="imgs" className=" bg-lightBlue h-[470px] rounded-full w-[500px] mx-auto ">
-              <img className="w-[240px]" src={rectangle} alt="" />
+              <img className="w-[340px]" src={image24} alt="" />
               </div>
             </div>
           </div>
         </div>
-        
+
       </section>
       
+      <Work />
     </div>
   );
 };
---
+++ src/components/Home/Work.jsx
@@ -0,0 +1,16 @@
+
+const Work = () => {
+  return (
+    <section>
+        <div>
+        <p>How Flexa Works</p>
+        <p>Step-By-Step Process</p>
+        </div>
+        <div>
+            
+        </div>
+    </section>
+  )
+}
+
+export default Work
