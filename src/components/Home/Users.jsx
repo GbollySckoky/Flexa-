f86a539adcd1939555edab2aeab85c76aecb9c3a
Coker <gbolahancoker99@gmail.com>
2024-01-02 11:18:42 +0100

    Flexa_Project

--- src/components/Home/Users.jsx
+++ src/components/Home/Users.jsx
@@ -2,9 +2,13 @@
 
 const Users = () => {
   return (
-    <div>
-        
-    </div>
+    <section>
+        <div>
+            <h1>See What Our Users Has To Say</h1>
+            <p>Every month, we bring the spotlight to the heart of Flexa`s dynamic financial ecosystem, 
+            unveiling the comprehensive stories of individuals who have harnessed the full spectrum of Flexa`s capabilities.</p>
+        </div>
+    </section>
   )
 }
 
--- src/components/Home/Work.jsx
+++ src/components/Home/Work.jsx
@@ -1,52 +1,56 @@
 import { FaRegUser } from "react-icons/fa";
 import { CiWallet } from "react-icons/ci";
 import { FiLock } from "react-icons/fi";
+import Users from "./Users";
 
 const Work = () => {
   return (
-    <section className="bg-Blue mt-[60px] ">
-      <div id="container" className="py-[80px] ">
-        <div className="text-white">
-          <p className="pb-2">How Flexa Works</p>
-          <p className="pb-5 text-3xl font-medium">Step-By-Step Process</p>
+    <>
+      <section className="bg-Blue mt-[60px] ">
+        <div id="container" className="py-[80px] ">
+          <div className="text-white">
+            <p className="pb-2">How Flexa Works</p>
+            <p className="pb-5 text-3xl font-medium">Step-By-Step Process</p>
+          </div>
+          <section className="grid grid-cols-3 gap-3">
+            {/* CREATE YOUR FLEXA ACCOUNT */}
+
+            <div className="bg-lightBlue text-white px-[30px] pt-[40px] pb-[80px]">
+              <FaRegUser className="py-5 text-7xl bg-white text-yellows rounded-full" />
+              <h1 className="py-[20px]">Create Your Flexa Account </h1>
+              <p>
+                Begin your journey by creating a Flexa account. Simply sign up
+                with your email, create a secure password, and you`re ready to
+                go.
+              </p>
+            </div>
+
+            {/* MANAGE YOUR WALLET */}
+
+            <div className="bg-lightBlue text-white px-[30px] pt-[40px] pb-[80px]">
+              <CiWallet className="py-5 text-7xl  bg-white text-yellows rounded-full" />
+              <h1 className="py-[20px]">Manage Your Wallet </h1>
+              <p>
+                Navigate to the wallet section to view your current balance,
+                transaction breakdowns. and manage your funds.
+              </p>
+            </div>
+
+            {/* SECURE YOUR ACCOUNT */}
+
+            <div className="bg-lightBlue text-white px-[30px] pt-[40px] pb-[80px]">
+              <FiLock className="py-5 text-7xl bg-white text-yellows rounded-full" />
+              <h1 className="py-[20px]">Manage Your Wallet </h1>
+              <p>
+                Navigate to the wallet section to view your current balance,
+                transaction breakdowns. and manage your funds.
+              </p>
+            </div>
+          </section>
         </div>
-        <section className="grid grid-cols-3 gap-3">
-
-          {/* CREATE YOUR FLEXA ACCOUNT */}
-
-          <div className="bg-lightBlue text-white px-[30px] pt-[40px] pb-[80px]">
-            <FaRegUser className="py-5 text-7xl bg-white text-yellows rounded-full" />
-            <h1 className="py-[20px]">Create Your Flexa Account </h1>
-            <p>
-              Begin your journey by creating a Flexa account. Simply sign up
-              with your email, create a secure password, and you`re ready to go.
-            </p>
-          </div>
-
-          {/* MANAGE YOUR WALLET */}
-
-          <div className="bg-lightBlue text-white px-[30px] pt-[40px] pb-[80px]">
-            <CiWallet className="py-5 text-7xl  bg-white text-yellows rounded-full" />
-            <h1 className="py-[20px]">Manage Your Wallet </h1>
-            <p>
-              Navigate to the wallet section to view your current balance,
-              transaction breakdowns. and manage your funds.
-            </p>
-          </div>
-
-          {/* SECURE YOUR ACCOUNT */}
-
-          <div className="bg-lightBlue text-white px-[30px] pt-[40px] pb-[80px]">
-            <FiLock className="py-5 text-7xl bg-white text-yellows rounded-full" />
-            <h1 className="py-[20px]">Manage Your Wallet </h1>
-            <p>
-              Navigate to the wallet section to view your current balance,
-              transaction breakdowns. and manage your funds.
-            </p>
-          </div>
-        </section>
-      </div>
-    </section>
+      </section>
+      <Users />
+    </>
   );
 };
 
