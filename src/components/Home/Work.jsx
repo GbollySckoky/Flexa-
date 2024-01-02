dea00669b307c3ab3c96c5907700d123a9a4893a
Coker <gbolahancoker99@gmail.com>
2024-01-02 06:25:47 +0100

    Flexa_Project

--- src/components/Home/Work.jsx
+++ src/components/Home/Work.jsx
@@ -5,38 +5,47 @@ import { FiLock } from "react-icons/fi";
 const Work = () => {
   return (
     <section className="bg-Blue mt-[60px] ">
-        <div id="container" className="pt-[70px]">
-        <div>
-        <p className="pb-2">How Flexa Works</p>
-        <p className="pb-5">Step-By-Step Process</p>
+      <div id="container" className="py-[80px] ">
+        <div className="text-white">
+          <p className="pb-2">How Flexa Works</p>
+          <p className="pb-5 text-3xl font-medium">Step-By-Step Process</p>
+        </div>
+        <section className="grid grid-cols-3 gap-3">
+
+          {/* CREATE YOUR FLEXA ACCOUNT */}
+
+          <div className="bg-lightBlue text-white px-[30px] pt-[40px] pb-[80px]">
+            <FaRegUser className="py-5 text-7xl bg-white text-yellows rounded-full" />
+            <h1 className="py-[20px]">Create Your Flexa Account </h1>
+            <p>
+              Begin your journey by creating a Flexa account. Simply sign up
+              with your email, create a secure password, and you`re ready to go.
+            </p>
+          </div>
+
+          {/* MANAGE YOUR WALLET */}
+
+          <div className="bg-lightBlue text-white px-[30px] pt-[40px] pb-[80px]">
+            <CiWallet className="py-5 text-7xl  bg-white text-yellows rounded-full" />
+            <h1 className="py-[20px]">Manage Your Wallet </h1>
+            <p>
+              Navigate to the wallet section to view your current balance,
+              transaction breakdowns. and manage your funds.
+            </p>
+          </div>
+
+          {/* SECURE YOUR ACCOUNT */}
+
+          <div className="bg-lightBlue text-white px-[30px] pt-[40px] pb-[80px]">
+            <FiLock className="py-5 text-7xl bg-white text-yellows rounded-full" />
+            <h1 className="py-[20px]">Manage Your Wallet </h1>
+            <p>
+              Navigate to the wallet section to view your current balance,
+              transaction breakdowns. and manage your funds.
+            </p>
+          </div>
+        </section>
       </div>
-      <section className="flex gap-3">
-        <div className="bg-lightBlue">
-          <FaRegUser />
-          <h2>Create Your Flexa Account </h2>
-          <p>
-            Begin your journey by creating a Flexa account. Simply sign up with
-            your email, create a secure password, and you`re ready to go.
-          </p>
-        </div>
-        <div className="bg-lightBlue">
-          <CiWallet />
-          <h2>Manage Your Wallet </h2>
-          <p>
-            Navigate to the wallet section to view your current balance,
-            transaction breakdowns. and manage your funds.
-          </p>
-        </div>
-        <div className="bg-lightBlue">
-          <FiLock />
-          <h2>Manage Your Wallet </h2>
-          <p>
-            Navigate to the wallet section to view your current balance,
-            transaction breakdowns. and manage your funds.
-          </p>
-        </div>
-      </section>
-        </div>
     </section>
   );
 };
