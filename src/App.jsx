8369859bb6f53bbd9fbd2fbc830dcc2de2eca79b
Coker <gbolahancoker99@gmail.com>
2024-01-04 08:15:22 +0100

    Flexa_Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import Header from './components/Header/Header'
-import {BrowserRouter,Route,Routes} from 'react-router-dom'
-import Hero from './components/Home/Hero'
+import React from "react";
+import Header from "./components/Header/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Hero from "./components/Home/Hero";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <React.Fragment>
       <BrowserRouter>
-      <Header />
-      <Hero />
-
-      <Routes>
-        <Route  />
-      </Routes>
+        <Header />
+        <Hero />
+      
+        <Routes>
+          <Route />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,87 @@
-
+import { Link } from "react-router-dom";
+import logo from "../../assets/image 20.svg";
+import twitter from "../../assets/pajamas_twitter.svg"
+import ig from "../../assets/openmoji_instagram.svg"
+import behance  from "../../assets/uil_behance.svg"
+import linkden from "../../assets/ri_linkedin-fill.svg"
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <section id="container" className="bg-Blue ">
+      <div  className="flex justify-between pt-[80px] pb-[20px]">
+        <div className=" flex items-start ">
+          <img src={logo} alt="" />
+          <h1>Flexa</h1>
+        </div>
+        {/* PRODUCT */}
+        <nav>
+          <ul>
+            <li>
+              <Link to={"#"}>Products</Link>
+            </li>
+            <li>
+              <Link to={"#"}>Invest</Link>
+            </li>
+            <li>
+              <Link to={"#"}>Loans</Link>
+            </li>
+            <li>
+              <Link to={"#"}>Flexfinance</Link>
+            </li>
+            <li>
+              <Link to={"#"}>Target Savings</Link>
+            </li>
+          </ul>
+        </nav>
 
-export default Footer
+        {/* COMPANY */}
+        <ul>
+          <li>
+            <Link to={"#"}>Company</Link>
+          </li>
+          <li>
+            <Link to={"#"}>Invest</Link>
+          </li>
+          <li>
+            <Link to={"#"}>Loans</Link>
+          </li>
+          <li>
+            <Link to={"#"}>Flexfinance</Link>
+          </li>
+          <li>
+            <Link to={"#"}>Target Savings</Link>
+          </li>
+        </ul>
+
+        {/* LEGAL */}
+        <ul>
+          <li>
+            <Link to={"#"}>Legal</Link>
+          </li>
+          <li>
+            <Link to={"#"}>Invest</Link>
+          </li>
+          <li>
+            <Link to={"#"}>Loans</Link>
+          </li>
+          <li>
+            <Link to={"#"}>Flexfinance</Link>
+          </li>
+          <li>
+            <Link to={"#"}>Target Savings</Link>
+          </li>
+        </ul>
+      </div>
+
+      {/* MEDIAS */}
+      <div className=" flex pb-[80px]">
+        <img src={twitter} alt="" className="bg-grey rounded-full p-2 ml-2"/>
+        <img src={ig} alt="" className="bg-grey rounded-full p-2 ml-2"/>
+        <img src={behance} alt="" className="bg-grey rounded-full p-2 ml-2"/>
+        <img src={linkden} alt="" className="bg-grey rounded-full p-2 ml-2"/>
+      </div>
+      <hr />
+    </section>
+  );
+};
+
+export default Footer;
